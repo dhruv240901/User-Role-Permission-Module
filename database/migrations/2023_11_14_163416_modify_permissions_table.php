316f85998847976686a702acc92807c2011e5ba3
Dhruv531142 <65854604+Dhruv531142@users.noreply.github.com>
2023-11-14 22:44:08 +0530

    modify migrations and seeders

---
+++ database/migrations/2023_11_14_163416_modify_permissions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->foreign('created_by')->references('id')->on('users')
+            ->onDelete('RESTRICT')->onUpdate('RESTRICT');
+            $table->foreign('updated_by')->references('id')->on('users')
+            ->onDelete('RESTRICT')->onUpdate('RESTRICT');
+            $table->foreign('deleted_by')->references('id')->on('users')
+            ->onDelete('RESTRICT')->onUpdate('RESTRICT');
+            $table->boolean('is_deleted')->default(0)->comment("0:No,1:Yes");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->foreign('created_by')->references('id')->on('users')
+            ->onDelete('RESTRICT')->onUpdate('RESTRICT');
+            $table->foreign('updated_by')->references('id')->on('users')
+            ->onDelete('RESTRICT')->onUpdate('RESTRICT');
+            $table->foreign('deleted_by')->references('id')->on('users')
+            ->onDelete('RESTRICT')->onUpdate('RESTRICT');
+            $table->boolean('is_deleted')->default(0)->comment("0:No,1:Yes");
+        });
+    }
+};
--- database/seeders/ModuleSeeder.php
+++ database/seeders/ModuleSeeder.php
@@ -14,12 +14,12 @@ class ModuleSeeder extends Seeder
     public function run(): void
     {
         $modules = [
-            ['id'=>'8eef54cf-349a-4614-84a4-1bca89b8afd9','module_code'=>'Acc','name'=>'Account'],
-            ['id'=>'c1041e66-ed16-499c-ba6a-a04e514f7475','module_code'=>'AL','name'=>'Activity Log'],
-            ['id'=>'f1f9c6b1-b0da-409a-8c78-ba35e7800d6b','module_code'=>'FL','name'=>'File'],
-            ['id'=>'9a25308d-6a16-4312-bc6f-0616c6d75159','module_code'=>'PER','name'=>'Permission'],
-            ['id'=>'1516e033-b202-4feb-9644-93c9d2163645','module_code'=>'RO','name'=>'Role'],
-            ['id'=>'4292ce5a-a33f-4945-aa27-d29262faa54c','module_code'=>'US','name'=>'User'],
+            ['id'=>'8eef54cf-349a-4614-84a4-1bca89b8afd9','module_code'=>'Acc','name'=>'Account','parent_id'=>null],
+            ['id'=>'c1041e66-ed16-499c-ba6a-a04e514f7475','module_code'=>'AL','name'=>'Activity Log','parent_id'=>'8eef54cf-349a-4614-84a4-1bca89b8afd9'],
+            ['id'=>'f1f9c6b1-b0da-409a-8c78-ba35e7800d6b','module_code'=>'FL','name'=>'File','parent_id'=>'8eef54cf-349a-4614-84a4-1bca89b8afd9'],
+            ['id'=>'9a25308d-6a16-4312-bc6f-0616c6d75159','module_code'=>'PER','name'=>'Permission','parent_id'=>'8eef54cf-349a-4614-84a4-1bca89b8afd9'],
+            ['id'=>'1516e033-b202-4feb-9644-93c9d2163645','module_code'=>'RO','name'=>'Role','parent_id'=>'8eef54cf-349a-4614-84a4-1bca89b8afd9'],
+            ['id'=>'4292ce5a-a33f-4945-aa27-d29262faa54c','module_code'=>'US','name'=>'User','parent_id'=>'8eef54cf-349a-4614-84a4-1bca89b8afd9'],
        ];
 
        Module::insert($modules);
